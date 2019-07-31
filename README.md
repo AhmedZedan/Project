fccf45473cadcc47eec82f0aef9a0ce8fc15ebe6
AhmedZedan <a.zidan19@yahoo.com>
2019-07-31 10:52:20 +0200

    init git and add all files

---
+++ README.md
@@ -0,0 +1,3 @@
+Peer-graded Assignment: Bash, Make, Git, and GitHub
+ر يول 31 10:47:15 EET 2019
+24
---
+++ guessinggame.sh
@@ -0,0 +1,24 @@
+number_of_files=$(ls -1 | wc -l)
+echo "How many files in this directory you think?!"
+read number
+echo $number
+
+if [[ $number -eq $number_of_files ]]
+then
+echo "Congratolation you are right"
+fi
+
+while [[ $number != $number_of_files ]]
+do
+if [[ $number -gt $number_of_files ]]
+then
+echo "The number of files smaller than the number you entered"
+elif [[ $number -lt $number_of_files ]]
+then
+echo "The number of files bigger than the number you entered"
+fi
+echo "Enter another number"
+read number
+continue
+done
+echo "congratolation you are right"
